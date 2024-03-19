254812fa350603851faab4c1dcebc4d31671e204
Jose Rubio <jadrubio@gmail.com>
2024-03-19 15:07:39 -0400

    added in chi and basic handler and route files

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,5 +1,28 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+const port = 8080
+
+type application struct {
+	Domain string
+}
+
 func main() {
+	var app application
+
+	app.Domain = "example.com"
+
+	log.Println("Starting application server on port", port)
+
+	err := http.ListenAndServe(fmt.Sprintf(":%d", port), app.routes())
+
+	if err != nil {
+		log.Fatal(err)
+	}
 
 }
