810aa1f845c980aca71ca84934009aed5af03097
Jose Rubio <jadrubio@gmail.com>
2024-03-20 12:01:56 -0400

    connecting to docker from app

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"database/sql"
+	"flag"
 	"fmt"
 	"log"
 	"net/http"
@@ -10,16 +12,29 @@ const port = 8080
 
 type application struct {
 	Domain string
+	DSN    string
+	DB     *sql.DB
 }
 
 func main() {
 	var app application
 
+	flag.StringVar(&app.DSN, "dsn", "host=localhost port=5432 user=postgres password=postgres dbname=movies sslmode=disable timezone=UTC connect_timeout=5", "Postgres connection string")
+	flag.Parse()
+
+	conn, err := app.connectToDB()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	app.DB = conn
+	defer app.DB.Close()
+
 	app.Domain = "example.com"
 
 	log.Println("Starting application server on port", port)
 
-	err := http.ListenAndServe(fmt.Sprintf(":%d", port), app.routes())
+	err = http.ListenAndServe(fmt.Sprintf(":%d", port), app.routes())
 
 	if err != nil {
 		log.Fatal(err)
