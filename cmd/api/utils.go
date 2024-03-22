dc988817013f24cf349c6dfa2b25ef20bfb1a51d
Jose Rubio <jadrubio@gmail.com>
2024-03-22 14:09:16 -0400

    created util func to writing JSON error messages

--- cmd/api/utils.go
+++ cmd/api/utils.go
@@ -13,6 +13,20 @@ type JSONResponse struct {
 	Data    interface{} `json:"data,omitempty"`
 }
 
+func (app *application) errorJSON(w http.ResponseWriter, err error, status ...int) error {
+	statusCode := http.StatusBadRequest
+
+	if len(status) > 0 {
+		statusCode = status[0]
+	}
+
+	var payload JSONResponse
+	payload.Error = true
+	payload.Message = err.Error()
+
+	return app.writeJSON(w, statusCode, payload)
+}
+
 func (app *application) readJSON(w http.ResponseWriter, r *http.Request, data interface{}) error {
 	maxBytes := 1024 * 1024
 	r.Body = http.MaxBytesReader(w, r.Body, int64(maxBytes))
