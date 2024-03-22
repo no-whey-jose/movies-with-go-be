2c447e9a336054ee45bf9e5ee6cd8905afc23bbb
Jose Rubio <jadrubio@gmail.com>
2024-03-22 10:02:14 -0400

    created util func to write JSON responses

--- cmd/api/handlers.go
+++ cmd/api/handlers.go
@@ -1,11 +1,8 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"movies-be/internal/models"
 	"net/http"
-	"time"
 )
 
 func (app *application) Home(w http.ResponseWriter, r *http.Request) {
@@ -19,52 +16,16 @@ func (app *application) Home(w http.ResponseWriter, r *http.Request) {
 		Version: "1.0.0",
 	}
 
-	out, err := json.Marshal(payload)
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	w.Write(out)
+	_ = app.writeJSON(w, http.StatusOK, payload)
 }
 
 func (app *application) AllMovies(w http.ResponseWriter, r *http.Request) {
-	var movies []models.Movie
 
-	rd, _ := time.Parse("2006-01-02", "2013-10-05")
-
-	walterMitty := models.Movie{
-		ID:          1,
-		Title:       "The Secret Life of Walter Mitty",
-		ReleaseDate: rd,
-		RunTime:     114,
-		Rated:       "PG",
-		Description: "When both he and a colleague are about to lose their job, Walter takes action by embarking on an adventure more extraordinary than anything he ever imagined.",
-	}
-
-	movies = append(movies, walterMitty)
-
-	rd, _ = time.Parse("2006-01-02", "1995-09-22")
-
-	empireRecords := models.Movie{
-
-		ID:          2,
-		Title:       "Empire Records",
-		ReleaseDate: rd,
-		RunTime:     90,
-		Rated:       "PG-13",
-		Description: "A day in the life of the employees of Empire Records. Except this is a day where everything comes to a head for a number of them facing personal crises - can they pull through together? And more importantly, can they keep their record store independent and not swallowed up by corporate greed?",
-	}
-
-	movies = append(movies, empireRecords)
-
-	out, err := json.Marshal(movies)
+	movies, err := app.DB.AllMovies()
 	if err != nil {
 		fmt.Println(err)
+		return
 	}
 
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	w.Write(out)
+	_ = app.writeJSON(w, http.StatusOK, movies)
 }
